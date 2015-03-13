8c7aae8ca63cc45605c683208ec60fa7bf4861fc
nbermudez <nbermudez@Blizzard.com>
2015-03-12 18:05:01 -0700

    Initial Code

---
+++ signs/birthday.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signs
+{
+    public class Birthday
+    {
+        public int dobMonth;
+        public int dobDay;
+        public int dobYear;
+
+        static void Main(string[] args)
+        {
+            
+            Birthday birthday = new Birthday();
+
+            DateTime today = DateTime.Today; 
+
+            Console.Write("What year were you born in? (ex. 2000) ");
+            birthday.dobYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What month were you born in? (ex. 11) ");
+            birthday.dobMonth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What day were you born on? (ex. 11) ");
+            birthday.dobDay = Convert.ToInt32(Console.ReadLine());
+
+            DateTime birthdayDate = new DateTime(birthday.dobYear, birthday.dobMonth, birthday.dobDay);
+            String.Format("{0:M/d/yyy}", birthday);
+            Console.WriteLine("Your birthday is on: " + birthday);
+
+            //check to see if birthday is today
+            if (today == birthdayDate)
+            {
+                Console.WriteLine("HAPPY BIRTHDAY!!!! ");
+            }
+
+            //find age of user (years)
+            TimeSpan actualAge = today - birthdayDate;
+            int age = Convert.ToInt32(actualAge.TotalDays) / 365;
+
+            //Check if age is possible, not born yet or > 120 ,output if error
+            if (age <= 0)
+            {
+                Console.WriteLine("You are not born yet. Thank you for trying. :)");
+            }else if (age > 120)
+            {
+                Console.WriteLine("Noone has ever lived this long. Did you make a mistake?");    
+            }
+
+
+
+            //output age
+            Console.WriteLine("You are " + age + " years old!");
+            
+            
+
+
+            //output users Astrological signs , western and chinese
+            MoonSigns moonsigns = new MoonSigns();
+
+            moonsigns.MoonSignReturn();
+          
+            
+           // Console.WriteLine("Your Astrological Sign is " + aSign);
+
+            string zodiac = "";
+
+            switch ((birthday.dobYear - 4) % 12) 
+            {
+                case  0: 
+                    zodiac = "Rat";       
+                    break;
+                case  1: 
+                    zodiac = "Ox";            
+                    break;
+                case  2: 
+                    zodiac = "Tiger";     
+                    break;
+                case  3: 
+                    zodiac = "Rabbit";    
+                    break;
+                case  4: 
+                    zodiac = "Dragon";    
+                    break;
+                case  5: 
+                    zodiac = "Snake";     
+                    break;
+                case  6: 
+                    zodiac = "Horse";     
+                    break;
+                case  7: 
+                    zodiac = "Goat";  
+                    break;
+                case  8: 
+                    zodiac = "Monkey";    
+                    break;
+                case  9: 
+                    zodiac = "Rooster";   
+                    break;
+                case 10: 
+                    zodiac = "Dog";   
+                    break;
+                case 11: 
+                    zodiac = "Pig";   
+                    break;
+                default:
+                    Console.WriteLine("You have sign buddy!");
+                    break;
+            }
+
+            Console.WriteLine("Your Chinese Sign is " + zodiac);
+        }
+    }
+}
---
+++ signs/moonSign.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Signs
+{
+    public class MoonSigns
+    {
+        public static void moonSignReturn()
+        {
+            string aSign = "";
+            Birthday birthday = new Birthday();
+            
+            switch (birthday.dobMonth)
+          {
+                case 1:
+                    if ( birthday.dobDay > 20)
+                    {
+                        aSign = "Aquarius";
+                    }
+                    else
+                    {
+                        aSign = "Capricorn";
+                    }
+                    break;
+                case 2:
+                    if (birthday.dobDay > 18)
+                    {
+                        aSign = "Pisces";
+                    }
+                    else
+                    {
+                        aSign = "Aquarius";
+                    }
+                    break;
+                case 3:
+                    if (birthday.dobDay > 20)
+                    {
+                        aSign = "Aries";
+                    }
+                    else
+                    {
+                        aSign = "Pisces";
+                    }
+                    break;
+                case 4:
+                    if (birthday.dobDay > 19)
+                    {
+                        aSign = "Taurus";
+                    }
+                    else
+                    {
+                        aSign = "Aries";
+                    }
+                    break;
+                case 5:
+                    if (birthday.dobDay > 20)
+                    {
+                        aSign = "Gemini";
+                    }
+                    else
+                    {
+                        aSign = "Taurus";
+                    }
+                    break;
+                case 6:
+                    if (birthday.dobDay > 20)
+                    {
+                        aSign = "Cancer";
+                    }
+                    else
+                    {
+                        aSign = "Gemini";
+                    }
+                    break;
+                case 7:
+                    if (birthday.dobDay > 22)
+                    {
+                        aSign = "Leo";
+                    }
+                    else
+                    {
+                        aSign = "Cancer";
+                    }
+                    break;
+                case 8:
+                    if (birthday.dobDay > 22)
+                    {
+                        aSign = "Virgo";
+                    }
+                    else
+                    {
+                        aSign = "Leo";
+                    }
+                    break;
+                case 9:
+                    if (birthday.dobDay > 22)
+                    {
+                        aSign = "Libra";
+                    }
+                    else
+                    {
+                        aSign = "Virgo";
+                    }
+                    break;
+                case 10:
+                    if (birthday.dobDay > 22)
+                    {
+                        aSign = "Scorpio";
+                    }
+                    else
+                    {
+                        aSign = "Libra";
+                    }
+                    break;
+                case 11:
+                    if (birthday.dobDay > 21)
+                    {
+                        aSign = "Sagittarius";
+                    }
+                    else
+                    {
+                        aSign = "Scorpio";
+                    }
+                    break;
+                case 12:
+                    if (birthday.dobDay > 20)
+                    {
+                        aSign = "Capricorn";
+                    }
+                    else
+                    {
+                        aSign = "Sagittarius";
+                    }
+                    break;
+                default:
+                    Console.WriteLine("INVALID DATE!!");
+                    break;
+               
+
+             }//end switch
+           
+    }//end class
+    }
+}
