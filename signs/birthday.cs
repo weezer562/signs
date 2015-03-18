67dbf5adfb63b932c20f14e9ec301793ba71a86a
nbermudez <nbermudez@Blizzard.com>
2015-03-18 11:20:35 -0700

    Added cSigns

--- signs/birthday.cs
+++ signs/birthday.cs
@@ -63,53 +63,9 @@ namespace Signs
           
             
            // Console.WriteLine("Your Astrological Sign is " + aSign);
-
-            string zodiac = "";
-
-            switch ((birthday.dobYear - 4) % 12) 
-            {
-                case  0: 
-                    zodiac = "Rat";       
-                    break;
-                case  1: 
-                    zodiac = "Ox";            
-                    break;
-                case  2: 
-                    zodiac = "Tiger";     
-                    break;
-                case  3: 
-                    zodiac = "Rabbit";    
-                    break;
-                case  4: 
-                    zodiac = "Dragon";    
-                    break;
-                case  5: 
-                    zodiac = "Snake";     
-                    break;
-                case  6: 
-                    zodiac = "Horse";     
-                    break;
-                case  7: 
-                    zodiac = "Goat";  
-                    break;
-                case  8: 
-                    zodiac = "Monkey";    
-                    break;
-                case  9: 
-                    zodiac = "Rooster";   
-                    break;
-                case 10: 
-                    zodiac = "Dog";   
-                    break;
-                case 11: 
-                    zodiac = "Pig";   
-                    break;
-                default:
-                    Console.WriteLine("You have sign buddy!");
-                    break;
-            }
-
-            Console.WriteLine("Your Chinese Sign is " + zodiac);
+            CNSigns cnsigns = new CNSigns();
+            cnsigns.cnSignReturn(birthday.dobYear);
+           
         }
     }
 }
---
+++ signs/cnSign.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Signs
+{
+    public class CNSigns
+    {
+        public void cnSignReturn(int year)
+        {
+            string zodiac = "";
+
+            switch ((year- 4) % 12)
+            {
+                case 0:
+                    zodiac = "Rat";
+                    break;
+                case 1:
+                    zodiac = "Ox";
+                    break;
+                case 2:
+                    zodiac = "Tiger";
+                    break;
+                case 3:
+                    zodiac = "Rabbit";
+                    break;
+                case 4:
+                    zodiac = "Dragon";
+                    break;
+                case 5:
+                    zodiac = "Snake";
+                    break;
+                case 6:
+                    zodiac = "Horse";
+                    break;
+                case 7:
+                    zodiac = "Goat";
+                    break;
+                case 8:
+                    zodiac = "Monkey";
+                    break;
+                case 9:
+                    zodiac = "Rooster";
+                    break;
+                case 10:
+                    zodiac = "Dog";
+                    break;
+                case 11:
+                    zodiac = "Pig";
+                    break;
+                default:
+                    Console.WriteLine("You have sign buddy!");
+                    break;
+            }
+
+            Console.WriteLine("Your Chinese Sign is " + zodiac);
+        }//end class
+    }
+}
